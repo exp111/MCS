86e0e64f81d96ee558b8854e319ce6511b14058a
Exp <theexp111@gmail.com>
2019-05-09 11:24:00 +0200

    Oof

--- P2.asm
+++ P2.asm
@@ -2,7 +2,7 @@
 ; Required USER PROGRAM BODY  when using  MCSsetupASM.S               24.05.2012 Kzy
 ;
 
-	.AREA	Memory (abs)
+.AREA	Memory (abs)
 STACK	=	$1000		; Nach RESET $1000, nach SetUp $2000=(End of RAM)+1
 SetUp	=	$F800		; Suboutine System SetUp
 
@@ -41,8 +41,6 @@ START:  LDS #STACK
         BSET DDRA, #BIT3
         BSET DDRA, #BIT4
         MOVB #0b0, DDRB ; PB0 as Input
-        MOVB #0b10000000, SUCCSTEP
-        MOVB #0b00000000, UE
         LDX #0xFFFE ; Init LCD
         JSR LCD4x20A
         LDX #LCDEI ; Init First Line (ADU Wert)
@@ -53,18 +51,23 @@ SPIINI: BSET PORTA, #BIT3
         MOVB #0b00000000, SPIBR ; 8MHz
         
         
-MLOOP:  JSR TRIGGER
-        
-        JSR INCUE ; UE = UE + STEP
-        LDY  #0x0308 ; Position
-        LDAA UE ; Volt Value
-        JSR VoltAnz
-        
+MLOOP:  MOVB #0b10000000, SUCCSTEP ; Reset/Init Values
+        MOVB #0b00000000, UE
+
+        JSR TRIGGER
+SUCCLOP:JSR INCUE ; UE = UE + STEP
         LDAA UE ; Read current value into akka
         JSR DAOUT ; Output akka
         JSR Wait ; Wait 1 ms
-          
         JSR SUCC
+        LDAA SUCCSTEP
+        CMPA #0
+        BNE SUCCLOP ; JMP if mask != 0 -> its still going on
+        
+        ; Output the end value to display
+        LDY  #0x0308 ; Position
+        LDAA UE ; Volt Value
+        JSR VoltAnz
         
         BRA MLOOP
         
@@ -77,13 +80,8 @@ TRIGGER:LDAA #0 ; 0V ausgeben
         JSR Wait
         RTS
         
-SUCC:   LDAA SUCCSTEP
-        CMPA #0
-        BEQ MLOOP ; already there
-       
-        LDAA PORTB
-        BRCLR PORTB, #BIT0, ELSEN ; cmp to PB0
-        BRA NEXT
+SUCC:   LDAA PORTB
+        BRSET PORTB, #BIT0, NEXT ; cmp to PB0
 ELSEN:  LDAA UE
         SUBA SUCCSTEP; larger than -> remove bit
         STAA UE
